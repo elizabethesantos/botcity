a6782b4cf5fe714d1899c7342a8c6dea857f4fa7
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2024-01-22 15:55:36 -0300

    orchestrador

--- bot-consulta-cep/bot.py
+++ bot-consulta-cep/bot.py
@@ -7,14 +7,27 @@ from botcity.maestro import *
 # Desabilitando os erros caso não estivermos conectados ao Maestro
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
-cep = "28633-530"
-
 def main():
     try:
         # Instanciando o BotMaestroSDK através dos argumentos passados pelo Runner
         maestro = BotMaestroSDK.from_sys_args()
         # Obtendo as informações da execução atual
         execution = maestro.get_execution()
+
+        # Obtendo credenciais do Maestro
+        usuario = maestro.get_credential("dados-login", "usuario")
+        senha = maestro.get_credential("dados-login", "senha")
+
+        # Obtendo parâmetro da tarefa
+        cep = execution.parameters.get("cep")
+
+        # Enviando alerta para o Maestro
+        maestro.alert(
+            task_id=execution.task_id,
+            title="Iniciando processo",
+            message=f"O processo de consulta foi iniciado - CEP: {cep}",
+            alert_type=AlertType.INFO
+        )
 
         # Instanciando WebDriver
         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -37,8 +50,26 @@ def main():
         # Convertendo dados da tabela para um dicionário
         tabela_resultados = table_to_dict(table=tabela_resultados)[0]
 
+        # Criando uma nova entrada de log
+        maestro.new_log_entry(
+            activity_label="consulta-cep",
+            values={
+                "cep": cep,
+                "logradouro": tabela_resultados['logradouronome'],
+                "bairro": tabela_resultados['bairrodistrito'],
+                "localidade": tabela_resultados['localidadeuf']
+            }
+        )
+
         print(tabela_resultados)
         driver.save_screenshot("resultado.png")
+
+        # Subindo arquivo de resultados
+        maestro.post_artifact(
+            task_id=execution.task_id,
+            artifact_name=f"Resultados-CEP-{cep}.png",
+            filepath="resultado.png"
+        )
 
         # Finalizando tarefa no Maestro
         maestro.finish_task(
@@ -47,9 +78,24 @@ def main():
             message="Task finalizada!"
         )
 
+
     except Exception as erro:
         print(f"Erro inesperado: {erro}")
         driver.save_screenshot("erro.png")
+
+    # Reportando erro ao Maestro
+        maestro.error(
+            task_id=execution.task_id,
+            exception=erro,
+            screenshot="erro.png"
+        )
+       
+        # Finalizando tarefa no Maestro como falha
+        maestro.finish_task(
+            task_id=execution.task_id,
+            status=AutomationTaskFinishStatus.FAILED,
+            message="Task falhou!"
+        )
 
     finally:
         # Finalizando o navegador
